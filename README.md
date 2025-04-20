1eae5532b531685dc6bd5ccf4cd77329db90d530
Andres Rodriguez <anmrodriguezga@unal.edu.co>
2025-04-20 17:18:47 -0500

    Added components for CV sections.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Anmrodriguezga.Github.Io
+# anmrodriguezga.Github.Io
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.5.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkillsComponent,
+    ContactComponent,
+    ExperienceComponent,
+    EducationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
